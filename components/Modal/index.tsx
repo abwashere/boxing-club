6593449dc66a9e32eff1f8830e980183d6251d54
abwashere <audrey.belson@gmail.com>
2023-03-14 16:54:21 +0100

    Fix modal display

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -12,10 +12,10 @@ const Modal = ({ isOpen, onClose, children }: Props) => {
   if (!isOpen) return null;
   return (
     <div
-      className='fixed inset-0 z-30 flex items-center justify-center p-2 bg-black bg-opacity-30 bg-blur-sm'
+      className='fixed inset-0 z-30 flex items-center justify-center p-2 bg-black tablet:p-12 bg-opacity-30 bg-blur-sm'
       onClick={onClose}
     >
-      <div className='w-[95%] flex flex-col items-center justify-center h-full'>
+      <div className='flex flex-col items-center justify-center w-full h-full'>
         <div
           className='mb-1 text-right text-white place-self-end'
           onClick={onClose}
