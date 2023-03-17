29d48472c9b40d8f09512e250e219f44e3f8117b
abwashere <audrey.belson@gmail.com>
2023-03-17 14:04:15 +0100

    Install ts-jest, Add config to test only test files

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-const nextJest = require('next/jest');
+import nextJest from 'next/jest';
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -18,8 +18,21 @@ const customJestConfig = {
     '^@/api/(.*)$': '<rootDir>/app/(user)/api/$1',
     '^@/studio/(.*)$': '<rootDir>/app/(studio)/studio/$1',
   },
-
+  testMatch: ['<rootDir>/**/*.{spec,test}.{js,jsx,ts,tsx}'],
   testEnvironment: 'jest-environment-jsdom',
+  transform: { '^.+\\.tsx?$': 'ts-jest' },
+  // coverageThreshold: {
+  //   global: {
+  //     statements: 70,
+  //     branches: 70,
+  //     functions: 70,
+  //     lines: 70,
+  //   },
+  // },
+  // collectCoverageFrom: [
+  //   '<rootDir>/**/*.{js,jsx,ts,tsx}',
+  //   '!<rootDir>/**/*.test.{js,jsx,ts,tsx}',
+  // ],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
