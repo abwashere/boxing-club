4bfa2cd853c30426ed1b1bf85e64642d82cc4f84
abwashere <audrey.belson@gmail.com>
2023-03-22 12:50:20 +0100

    Fix test config file, Change test script

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,13 @@
-import nextJest from 'next/jest';
+/* eslint-disable @typescript-eslint/no-var-requires */
+const nextJest = require('next/jest');
 
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-});
+const createJestConfig = nextJest({ dir: './' });
 
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const customJestConfig = {
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
 
-  // If your project is using Module Path Aliases, you will need to configure Jest to resolve the imports by matching the paths option in the tsconfig.json file with this moduleNameMapper option:
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^@/app/(.*)$': '<rootDir>/app/(user)/$1',
@@ -35,5 +31,4 @@ const customJestConfig = {
   // ],
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig);
